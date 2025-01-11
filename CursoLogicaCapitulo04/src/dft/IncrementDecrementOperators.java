92443d27aa17fd0b2583971d40fd296b5d63feac
romuloabbiati <rhabbiati@hotmail.com>
2025-01-11 17:37:06 +0000

    increment and decrement operators

---
+++ CursoLogicaCapitulo04/src/dft/IncrementDecrementOperators.java
@@ -0,0 +1,23 @@
+package dft;
+
+public class IncrementDecrementOperators {
+
+	public static void main(String[] args) {
+//		Integer numero = 10;
+//		System.out.println("Numero antes: " + numero);
+//		
+//		numero++;
+//		System.out.println("NUmero depois: " + numero);
+		
+//		Integer numero = 10;
+//		Integer numero02 = ++numero;
+//		Integer numero02 = numero++;
+//		System.out.println("Numero: " + numero + ", Numero02: " + numero02);
+		
+		Integer numero = 10;
+		--numero;
+		System.out.println("Numero: " + numero);
+
+	}
+
+}
