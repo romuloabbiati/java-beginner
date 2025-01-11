1d466c337a7320d9bf13a67265591b5447c0aefd
romuloabbiati <rhabbiati@hotmail.com>
2025-01-11 12:38:16 +0000

    logical operators

---
+++ CursoLogicaCapitulo04/src/dft/LogicalOperators.java
@@ -0,0 +1,29 @@
+package dft;
+
+public class LogicOperators {
+
+	public static void main(String[] args) {
+		Boolean carrinhoMaiorQue100 = true;
+		Boolean periodoDePromocao = false;
+		Boolean jaFezCompraNaLoja = true;
+		Boolean pagamentoAVista = true;
+		
+//		Boolean aplicarDesconto = carrinhoMaiorQue100 && periodoDePromocao;
+		
+//		Boolean aplicarDesconto = carrinhoMaiorQue100 || periodoDePromocao;
+		
+//		Boolean aplicarDesconto = periodoDePromocao && carrinhoMaiorQue100 && jaFezCompraNaLoja;
+		
+//		Boolean aplicarDesconto = periodoDePromocao || carrinhoMaiorQue100 || jaFezCompraNaLoja;
+		
+		Boolean aplicarDesconto = periodoDePromocao && (carrinhoMaiorQue100 || jaFezCompraNaLoja) && pagamentoAVista;
+		
+		if (aplicarDesconto) {
+			System.out.println("Sim! Aplique o desconto.");
+		} else {
+			System.out.println("Nao aplique o desconto.");
+		}
+
+	}
+
+}
