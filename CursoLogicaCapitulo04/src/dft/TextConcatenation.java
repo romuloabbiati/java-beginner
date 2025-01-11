5aed03194bf33ecdd23b54320afe9360d4989250
romuloabbiati <rhabbiati@hotmail.com>
2025-01-11 12:52:17 +0000

    text concatenation

---
+++ CursoLogicaCapitulo04/src/dft/TextConcatenation.java
@@ -0,0 +1,17 @@
+package dft;
+
+public class TextConcatenation {
+
+	public static void main(String[] args) {
+		String inicio = "Esse eh o ";
+		String meio = "o exemplo ";
+		String fim = "sobre concatenacao de textos.";
+		Integer numeroExemplo = 1;
+		
+		System.out.println(inicio + numeroExemplo + meio + fim);
+		
+//		System.out.println("Esse " + meio + "de concatenacao de textos.");
+
+	}
+
+}
