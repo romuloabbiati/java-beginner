2aa21a2a5ea37cb29b970671918fd63ca02da187
romuloabbiati <rhabbiati@hotmail.com>
2025-01-11 07:10:52 +0000

    module 4 exercicios 2

---
+++ CursoLogicaCapitulo04/src/dft/Operators.java
@@ -0,0 +1,28 @@
+package dft;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		Double number = 7.0;
+		
+//		number = number + 4;
+//		System.out.println("number + 4: " + number);
+		
+//		number += 4;
+//		System.out.println("number += 4: " + number);
+		
+//		number -= 4;
+//		System.out.println("number -= 4: " + number);
+		
+//		number *= 4;
+//		System.out.println("number *= 4: " + number);
+		
+//		number /= 4;
+//		System.out.println("number /= 4: " + number);
+		
+		number %= 4;
+		System.out.println("number %= 4: " + number);
+
+	}
+
+}
