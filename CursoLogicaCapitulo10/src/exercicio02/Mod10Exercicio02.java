ec13768de577b1ecb0aed35dbb98aceecefd56fe
romuloabbiati <rhabbiati@hotmail.com>
2025-01-23 08:08:00 +0000

    update on mod10 exercicio 2

--- CursoLogicaCapitulo10/src/exercicio02/Mod10Exercicio02.java
+++ CursoLogicaCapitulo10/src/exercicio02/Mod10Exercicio02.java
@@ -10,15 +10,23 @@ public class Mod10Exercicio02 {
 
 public static void main(String[] args) throws IOException {
 		
-		Path doc = Paths.get("/Users/romuloabbiati/Documents/java-logicaProgramacao/arquivo.txt");
-		List<String> tasks = Files.readAllLines(doc);
+		List<String> tasks = lerDoArquivo("/Users/romuloabbiati/Documents/java-logicaProgramacao/arquivo.txt");
+
+		System.out.println("This is your list of chores for the day: ");
 		
 		for (int i = 0; i < tasks.size(); i++) {
 			String task = tasks.get(i);
 			
 			System.out.println("Task " + (i + 1) + ": " + task);
 		}
+		
+		System.out.println("-----------------");
 
+	}
+
+	static List<String> lerDoArquivo(String arquivo) throws IOException {
+		Path doc = Paths.get(arquivo);
+		return  Files.readAllLines(doc);
 	}
 
 }
